8c4810a85d49b2906780cd56b87eb0850b29848b
VSZM <wayasam@gmail.com>
2015-12-22 02:42:27 -0800

    Added Day20 solution

---
+++ Advent_Of_Code_11-20/Day20_Houses.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Advent_Of_Code_11_20
+{
+    class Day20Houses : ISolvable
+    {
+        public string Solve(string[] inputLines, bool isPart2)
+        {
+            int N = int.Parse(inputLines[0]) / (isPart2 ? 11 : 10);
+            int[] houses = new int[N];
+
+            for (int i = 1; i < houses.Length; i++)
+            {
+                for (int j = i; j < houses.Length && (!isPart2 || j < 51 * i); j += i)
+                {
+                    houses[j] += i;
+                }
+            }
+
+            for (int i = 0; i < N; ++i)
+                if (houses[i] >= N)
+                    return i.ToString();
+
+            return "Should work doe..";
+        }
+    }
+}
--- Advent_Of_Code_11-20/Framework.cs
+++ Advent_Of_Code_11-20/Framework.cs
@@ -15,7 +15,7 @@ namespace Advent_Of_Code_11_20
 
             Stopwatch sw = new Stopwatch();
 
-            ISolvable solvable = new Day19Molecules();
+            ISolvable solvable = new Day20Houses();
 
             sw.Start();
 			string part1_solution = solvable.Solve( lines, false);
