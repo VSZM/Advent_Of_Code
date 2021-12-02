e962e078753fd8dd10fb8919ae3e273299347bb6
VSZM <wayasam@gmail.com>
2021-12-03 00:35:03 +0100

    - Day 2 Added

---
+++ 2021/AOC2021/Day2.cs
@@ -0,0 +1,61 @@
+﻿using System;
+namespace AOC2021
+{
+    public class Day2 : ISolvable
+    {
+        public Day2(string[] lines)
+        {
+            this.Instructions = lines;
+        }
+
+        public string[] Instructions { get; }
+
+        public string solve_part_1()
+        {
+            int depth = 0;
+            int h = 0;
+            foreach(var instruction in Instructions)
+            {
+                int num = int.Parse(instruction.Split()[1]);
+                if(instruction.StartsWith("forward"))
+                {
+                    h += num;
+                } else if (instruction.StartsWith("down"))
+                {
+                    depth += num;
+                } else
+                {
+                    depth -= num;
+                }
+            }
+
+            return (h * depth).ToString();
+        }
+
+        public string solve_part_2()
+        {
+            int depth = 0;
+            int h = 0;
+            int aim = 0;
+            foreach (var instruction in Instructions)
+            {
+                int num = int.Parse(instruction.Split()[1]);
+                if (instruction.StartsWith("forward"))
+                {
+                    h += num;
+                    depth += aim * num;
+                }
+                else if (instruction.StartsWith("down"))
+                {
+                    aim += num;
+                }
+                else
+                {
+                    aim -= num;
+                }
+            }
+
+            return (h * depth).ToString();
+        }
+    }
+}
--- 2021/AOC2021/Program.cs
+++ 2021/AOC2021/Program.cs
@@ -9,7 +9,7 @@ namespace AOC2021
         static void Main(string[] args)
         {
             var lines = File.ReadAllLines("input.txt");
-            var solver = new Day1(lines);
+            var solver = new Day2(lines);
             Console.WriteLine(string.Format("{0} Part 1 Solution: < {1} >", solver.GetType().Name, solver.solve_part_1()));
             Console.WriteLine(string.Format("{0} Part 2 Solution: < {1} >", solver.GetType().Name, solver.solve_part_2()));
         }
