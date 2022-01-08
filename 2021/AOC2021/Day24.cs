91e3a28f47c35fe1a80e913b40233132e63c4e25
VSZM <wayasam@gmail.com>
2022-01-08 01:05:53 +0100

    Day 24 recursive solution

--- 2021/AOC2021/Day24.cs
+++ 2021/AOC2021/Day24.cs
@@ -13,91 +13,63 @@ namespace AOC2021
         {
         }
 
+        static int[] div = new int[14] { 1, 1, 1, 1, 1, 26, 26, 1, 26, 1, 26, 26, 26, 26 };
+        static int[] check = new int[14] { 10, 10, 14, 11, 14, -14, 0, 10, -10, 13, -12, -3, -11, -2 };
+        static int[] offset = new int[14] { 2, 4, 8, 7, 12, 7, 10, 14, 2, 6, 8, 11, 5, 11 };
 
-        private bool Monad(long modelnumber)
+
+        // These are the states will always produce false
+        static HashSet<(int depth, int z)> SURELY_BAD_STATES = new HashSet<(int depth, int z)>();
+        
+
+        private long? GenerateModelNumber(int depth, long modelNumber, int z, int[] digits)
         {
-            var inputs = modelnumber.ToString().Reverse().Select(x => int.Parse(x.ToString())).ToList();
-            var div = new int[14] { 1, 1, 1, 1, 1, 26, 26, 1, 26, 1, 26, 26, 26, 26 };
-            var check = new int[14] { 10, 10, 14, 11, 14, -14, 0, 10, -10, 13, -12, -3, -11, -2 };
-            var offset = new int[14] { 2, 4, 8, 7, 12, 7, 10, 14, 2, 6, 8, 11, 5, 11 };
+            if (SURELY_BAD_STATES.Contains((depth, z)) || depth == 14)
+                return null;
 
-            int x = 0;
-            int y = 0;
-            int z = 0;
+            modelNumber *= 10;
+            var originalZ = z;
 
-
-            for (int i = 0; i < 14; i++)
+            for (int i = 0; i < digits.Length; ++i)
             {
-                int w = inputs[i];
-                x = z; // 0 
-                x %= 26; //0
-                z /= div[i]; // 0
-                x += check[i]; // x = 10
-                x = x == w ? 1 : 0; // x = 0
-                x = x == 0 ? 1 : 0; // x = 1
-                if (x != 0)
-                {
-                    y = 25;
-                    y *= x; // y = 25
-                    y += 1; // y = 26
-                    z *= y; // z = 0
-                    y = w;
-                    y += offset[i]; // y = w + 2
-                    y *= x;// THIS RIGHT here! If x == 0 then this entire iteration is NOOP
-                    z += y;
-                }
+                z = originalZ;
+                int w = digits[i];
+                var x = z;
+                x %= 26;
+                z /= div[depth];
+                x += check[depth];
+                x = x == w ? 1 : 0;
+                x = x == 0 ? 1 : 0;
+                var y = 25;
+                y *= x;
+                y += 1;
+                z *= y;
+                y = w;
+                y += offset[depth];
+                y *= x;
+                z += y;
+
+                if (z == 0 && depth == 13)
+                    return modelNumber + digits[i];
+                var ret = GenerateModelNumber(depth + 1, modelNumber + digits[i], z, digits);
+                if(ret != null)
+                    return ret;
+
             }
-            return z == 0;
-        }
 
+            SURELY_BAD_STATES.Add((depth, originalZ));
 
-        long GenerateNumber()
-        {
-            Random random = new Random();
-            long ret;
-            do
-            {
-                ret = random.NextInt64(11111111111111L, 99999999999999L);
-            } while (ret.ToString().Contains('0'));
-            return ret;
-        }
-
-        private long GreatestModelNumber()
-        {
-            long max = long.MinValue;
-            object mutex = new object();
-            Console.WriteLine("Progress: {0} %", 0);
-            Parallel.For(0L, 88888888888888L + 1L, new ParallelOptions { MaxDegreeOfParallelism = 32 }, (i, pls) =>
-            {
-                var number = 99999999999999L - i;
-                if (number.ToString().Contains('0'))
-                    return;
-                if (Monad(number))
-                {
-                    Console.WriteLine("Found Model number! {0}", number);
-                    lock (mutex)
-                    {
-                        if (number > max)
-                        {
-                            max = number;
-                            Console.WriteLine("New record model number found! {0}", number);
-                            pls.Break();
-                        }
-                    }
-                }
-            });
-
-            return max;
+            return null;
         }
 
         public object SolvePart1()
         {
-            return GreatestModelNumber();
+            return GenerateModelNumber(0, 0, 0, new int[] { 9, 8, 7, 6, 5, 4, 3, 2, 1});
         }
 
         public object SolvePart2()
         {
-            throw new NotImplementedException();
+            return GenerateModelNumber(0, 0, 0, new int[] { 1, 2, 3, 4, 5, 6, 7, 8, 9});
         }
     }
 }
